bfdbe7ce6e32edfde53ca2738bcf2f2807104851
Yusuke Shinyama <yusuke@shinyama.jp>
2016-06-03 16:35:35 +0900

    Initial commit.

---
+++ QueApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace QueApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll", EntryPoint="SetParent")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndParent);
+        private static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+        protected override void CreateHandle()
+        {
+            base.CreateHandle();
+            SetParent(Handle, HWND_MESSAGE);
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
